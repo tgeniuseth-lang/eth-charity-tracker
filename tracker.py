6ff0aa35020592656d2461f4ac5f21733dd15c12
tgeniuseth-lang <tgenius.eth@gmail.com>
2026-06-25 11:50:09 +0400

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -81,9 +81,14 @@ while True:
             for block_number in range(last_block + 1, current_block + 1):
                 block = w3.eth.get_block(block_number, full_transactions=True)
 
-                for tx in block.transactions:
-                    if tx.to and Web3.to_checksum_address(tx.to) == DONATION_WALLET:
-                        amount_eth = w3.from_wei(tx.value, "ether")
+               for tx in block.transactions:
+    if (
+        tx.to
+        and Web3.to_checksum_address(tx.to) == DONATION_WALLET
+        and tx["from"]
+        and Web3.to_checksum_address(tx["from"]) == TOKEN_ADDRESS
+    ):
+        amount_eth = w3.from_wei(tx.value, "ether")
 
                         if amount_eth > 0:
                             amount_eth = float(amount_eth)
